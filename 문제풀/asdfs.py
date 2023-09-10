f626110dbca16728b9aa86822a49891530346db2
Myung-Il <92520262+Myung-Il@users.noreply.github.com>
2023-09-11 01:42:32 +0900

    수 크기 최종 정리

--- 문제풀/asdfs.py
+++ 문제풀/asdfs.py
@@ -1,29 +1,24 @@
-import heapq
+nova,orgin=map(int,input().split())
 
-def sol():
-    for _ in range(int(input())):
-        k = int(input())
-        min_heap = []
-        max_heap = []
-               
-        for i in range(k):
-            op, num = input().split()
-            num = int(num)
-            if op == 'I':
-                heapq.heappush(min_heap, (num, i))
-                heapq.heappush(max_heap, (-num, i))
-            elif num == -1:
-                if min_heap:
-                    heapq.heappop(min_heap)
-            elif num == 1:
-                if max_heap:
-                    heapq.heappop(max_heap)
+nova_u=list(map(int,input().split()))
+orgin_u=list(map(int,input().split()))
 
+n_cnt,o_cnt=0,0
 
-        if min_heap:
-            print(-max_heap[0][0], min_heap[0][0])
-        else:
-            print('EMPTY')
-            
-if __name__ == "__main__":
-    sol()
+n=0
+s=0
+for i in nova_u:
+    if s>n:
+        n+=100
+        n_cnt+=1
+    s+=i
+
+o=0
+s=0
+for i in orgin_u:
+    if s>o:
+        o+=360
+        o_cnt+=1
+    s+=i
+
+print(n_cnt,o_cnt)
--- 문제풀/나나.py
+++ 문제풀/나나.py
@@ -1,69 +1,22 @@
-import heapq
-from sys import stdin
+import sys
+input = sys.stdin.readline
 
-
-def heapsort(iterable):
+n, m = map(int, input().split())
+a = sorted(map(int, input().split()))
+b = sorted(map(int, input().split()))
+cnt = 1
+j = a[0]
+for i in range(n):
+    if a[i] - j < 100: continue
+    cnt += 1
+    j = a[i]
     
-    h=[]
-    for i in iterable:
-        heapq.heappush(h,i)
-    for i in range(len(iterable)):
-        iterable[i]=heapq.heappop(h)
-    
-    return iterable
+print(cnt)
 
-
-input=stdin.readline
-
-for _ in range(int(input())):
-    q=[]
-
-    for _ in range(int(input())):
-        ty,imf=input().split()
-
-        if ty=='I':
-            q.append(int(imf))
-
-        elif ty=='D' and q:
-            q=heapsort(q)
-            q.pop(-1 if imf=='1'else 0)
-
-    if q:
-        print(max(q),min(q))
-    else:
-        print('EMPTY')
-
-
-'''
-1
-7
-I 16
-I -5643
-D -1
-D 1
-D 1
-I 123
-D -1
-
-1
-9
-I -45
-I 653
-D 1
-I -642
-I 45
-I 97
-D 1
-D -1
-I 333
-
-1
-7
-I 99
-I 98
-I 97
-D 1
-I 96
-I 99
-D -1
-'''
+cnt = 1
+j = b[0]
+for i in range(m):
+    if b[i] - j < 360: continue
+    cnt += 1
+    j = b[i]
+print(cnt)
