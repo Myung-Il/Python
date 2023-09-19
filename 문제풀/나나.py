3bc5551a15fa873d938c239818384591b019a766
Myung-Il <92520262+Myung-Il@users.noreply.github.com>
2023-09-19 11:30:41 +0900

    1520 골드 해결

--- 문제풀/나나.py
+++ 문제풀/나나.py
@@ -1,12 +1,65 @@
-n=int(input())
-a=[int(input())for _ in range(n)]
+import sys
+sys.setrecursionlimit(10 ** 8)
+input = sys.stdin.readline
 
-for i in range(1,n+1):
-    changed = False
-    for j in range(1,n-i):
-        if a[j-1]>a[j]:
-            changed = True
-            a[j-1],a[j]=a[j],a[j-1]
-    if not changed:
-        print(i)
-        break
+xn=[ 0, 0, 1,-1]
+yn=[ 1,-1, 0, 0]
+
+col,row=map(int,input().split())
+
+l=[list(map(int,input().split())) for _ in range(col)]
+v=[[-1 for _ in range(row)]for _ in range(col)]
+
+def dfs(x,y):
+    if y==col-1 and x==row-1:
+        return 1
+
+    if v[y][x]!=-1:
+        return v[y][x]
+
+    v[y][x]=0
+    for i in range(4):
+        xi=x+xn[i]
+        yi=y+yn[i]
+        if 0<=yi<col and 0<=xi<row and l[y][x]>l[yi][xi]:
+            v[y][x]+=dfs(xi,yi)
+
+    return v[y][x]
+
+print(dfs(0,0))
+
+'''
+3 4
+50 45 40 35 30
+99 40 99 30 99
+99 35 30 25 20
+-2
+
+4 5
+50 45 37 32 30
+35 50 40 20 25
+30 30 25 17 28
+27 24 22 15 10
+-3
+
+1 1
+1
+-1
+
+4 4
+93 72 61 58
+90 73 19 49
+85 36 75 13
+21 41 45 7
+-2
+
+7 7
+100 33 58 59 61 31 30
+74 31 55 62 70 70 29
+73 98 49 47 11 10 36
+62 59 56 45 44 8 7
+59 58 54 53 41 7 3
+56 32 29 18 40 4 3
+34 31 26 40 39 73 1
+-27
+'''
