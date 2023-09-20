ac137f6fddfd49ba17fa4fe6bd6534f4037c9d99
Myung-Il <92520262+Myung-Il@users.noreply.github.com>
2023-09-20 09:46:14 +0900

    1197 골드 4 해결

--- 문제풀/나나.py
+++ 문제풀/나나.py
@@ -1,11 +1,34 @@
 from sys import stdin
 input=lambda:stdin.readline().rstrip()
 
-v,e=map(int,input().split())
-l=sorted([list(map(int,input().split()))for _ in range(e)],key=lambda x:x[2])
 
-g=[l.pop(0)]
-for i in l:
-    a,b,c=i
+def find(u):
+    if u!=p[u]:
+        p[u]=find(p[u])
+    return p[u]
 
-    
+
+if __name__=='__main__':
+    v,e=map(int,input().split())
+    l=sorted([list(map(int,input().split()))for _ in range(e)],key=lambda x:x[2])
+    p=[i for i in range(v+1)]
+
+    ct=0
+    for a,b,c in l:
+        rt1=find(a)
+        rt2=find(b)
+        if rt1!=rt2:
+            if rt1>rt2:
+                p[rt1]=rt2
+            else:
+                p[rt2]=rt1
+            ct+=c
+
+    print(ct)
+
+'''
+3 3
+1 2 1
+2 3 2
+1 3 3
+'''
