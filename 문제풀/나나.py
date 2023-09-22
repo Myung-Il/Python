0311eeb54aa231f0ff29ef4838b8a8e2a033c3bb
Myung-Il <92520262+Myung-Il@users.noreply.github.com>
2023-09-22 16:23:25 +0900

    1251 실버 5 알고리즘 동아리

--- 문제풀/나나.py
+++ 문제풀/나나.py
@@ -1,37 +1,10 @@
-import heapq as h,sys
-input=lambda:sys.stdin.readline().rstrip()
-inf=float('inf')
+s=input()
+l=[]
 
-def dik(start):
-    q = []
-    h.heappush(q,[0, start])
-    dis_l[start] = 0
-    while q:
-        dis,exp_node=h.heappop(q)
-        if dis_l[exp_node]<dis:
-            continue
-        for n_exp_node,n_dis in graph[exp_node]:
-            dis_plus=dis+n_dis
-            if dis_plus<dis_l[n_exp_node]:
-                dis_l[n_exp_node]=dis_plus
-                h.heappush(q,[dis_plus,n_exp_node])
+r=len(s)
+for i in range(1,r-1):
+    for j in range(i+1,r):
+        find=s[:i][::-1]+s[i:j][::-1]+s[j:][::-1]
+        l.append(find)
 
-
-n,m,g=map(int,input().split())
-graph = [[]for _ in range(n+1)]
-for _ in range(m):
-    a,b,w=map(int,input().split())
-    graph[a].append([b,w])
-
-dis_l = [inf]*(n+1)
-dik(g)
-
-ans=dis_l[:]
-dis_l = [inf]*(n+1)
-for node in range(1,n+1):
-    if node!=g:
-        dik(node)
-        ans[node]+=dis_l[g]
-        dis_l = [inf]*(n+1)
-
-print(max(ans[1:]))
+print(sorted(l)[0])
