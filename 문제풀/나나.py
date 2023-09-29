6f3d24312d4f599f233e5633ccf87874c1d17ba5
Myung-Il <92520262+Myung-Il@users.noreply.github.com>
2023-09-29 13:56:10 +0900

    14226 골드 4

--- 문제풀/나나.py
+++ 문제풀/나나.py
@@ -5,34 +5,28 @@ input = lambda:stdin.readline().rstrip()
 
 
 def Bfs():
-    q = deque([[n,0]])
-    v = [-1]*100_001
+    q = deque([[1,1,1]])
+    v = [[True]*1_001 for _ in range(1001)]
 
     while q:
-        x,cnt = q.popleft()
-        if x == k:
+        x,c_ctrl,cnt = q.popleft()
+        if x == n:
             break
         
-        if -1<x-1<100_001 and v[x-1]==-1:
-            v[x-1] = x
-            q.append([x-1,cnt+1])
-        if -1<x+1<100_001 and v[x+1]==-1:
-            v[x+1] = x
-            q.append([x+1,cnt+1])
-        if -1<x*2<100_001 and v[x*2]==-1:
-            v[x*2] = x
-            q.append([x*2,cnt+1])
+        if 1<x<1_001 and v[x][x]:
+            q.append([x,x,cnt+1])
+            v[x][x] = False
+        if 1<x+c_ctrl<1_001 and c_ctrl>0 and v[x+c_ctrl][c_ctrl]:
+            q.append([x+c_ctrl,c_ctrl,cnt+1])
+            v[x+c_ctrl][c_ctrl] = False
+        if 1<x-1<1_001 and v[x-1]:
+            q.append([x-1,c_ctrl,cnt+1])
+            v[x-1][c_ctrl] = False
 
-    idx,l = k,[k]
-    for _ in range(cnt):
-        idx = v[idx]
-        l.append(idx)
-    l.reverse()
-    return cnt,l
+    return cnt
 
 
 if __name__=='__main__':
-    n,k = map(int,input().split())
-    time,r00t = Bfs()
+    n = int(input())
+    time = Bfs()
     print(time)
-    print(*r00t)
