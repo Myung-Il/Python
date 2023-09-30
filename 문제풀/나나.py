d7e5fe7cb5f283fe39b0a8831c57c5e2a1b255fe
Myung-Il <92520262+Myung-Il@users.noreply.github.com>
2023-09-30 09:31:20 +0900

    13549 골드 5

--- 문제풀/나나.py
+++ 문제풀/나나.py
@@ -5,28 +5,28 @@ input = lambda:stdin.readline().rstrip()
 
 
 def Bfs():
-    q = deque([[1,1,1]])
-    v = [[True]*1_001 for _ in range(1001)]
+    q = deque([[n,0]])
+    v = [-1]*100_001
 
     while q:
-        x,c_ctrl,cnt = q.popleft()
-        if x == n:
+        x,cnt = q.popleft()
+        if x == k:
             break
         
-        if 1<x<1_001 and v[x][x]:
-            q.append([x,x,cnt+1])
-            v[x][x] = False
-        if 1<x+c_ctrl<1_001 and c_ctrl>0 and v[x+c_ctrl][c_ctrl]:
-            q.append([x+c_ctrl,c_ctrl,cnt+1])
-            v[x+c_ctrl][c_ctrl] = False
-        if 1<x-1<1_001 and v[x-1]:
-            q.append([x-1,c_ctrl,cnt+1])
-            v[x-1][c_ctrl] = False
+        if -1<x*2<100_001 and v[x*2]==-1:
+            v[x*2] = x
+            q.append([x*2,cnt])
+        if -1<x-1<100_001 and v[x-1]==-1:
+            v[x-1] = x
+            q.append([x-1,cnt+1])
+        if -1<x+1<100_001 and v[x+1]==-1:
+            v[x+1] = x
+            q.append([x+1,cnt+1])
 
     return cnt
 
 
 if __name__=='__main__':
-    n = int(input())
+    n,k = map(int,input().split())
     time = Bfs()
     print(time)
