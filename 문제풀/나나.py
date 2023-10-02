e7de3aed48c1cd1cb0f6fafafadebe0ebf560b87
Myung-Il <92520262+Myung-Il@users.noreply.github.com>
2023-10-02 23:42:09 +0900

    3190 골드 4

--- 문제풀/나나.py
+++ 문제풀/나나.py
@@ -1,34 +1,49 @@
-import heapq,sys
-sys.setrecursionlimit(10**9)
+from collections import deque
+import sys
 input = lambda:sys.stdin.readline().rstrip()
 
 
-xn = [ 0, 0, 1,-1]
-yn = [ 1,-1, 0, 0]
-def Bfs():
-    hq = [[0,0,0]]
-    v = [[float('inf')]*row for _ in range(col)]
-
-    while hq:
-        x,y,bk = heapq.heappop(hq)
-        if x==row-1 and y==col-1:
-            return bk
-        
-        for i in range(4):
-            xi = x + xn[i]
-            yi = y + yn[i]
-            if 0<=xi<row and 0<=yi<col:
-                if l[yi][xi]==1:
-                    if v[yi][xi] > bk+1:
-                        v[yi][xi] = bk+1
-                        heapq.heappush(hq,[xi,yi,bk+1])
-                elif v[yi][xi] > bk:
-                    v[yi][xi] = bk
-                    heapq.heappush(hq,[xi,yi,bk])
+xn = [1, 0,-1, 0]
+yn = [0, 1, 0,-1]
+def f(c):
+    if c=='L':return(dir-1)%4
+    else:return(dir+1)%4
 
 
 if __name__=='__main__':
-    row,col = map(int,input().split())
-    l = [list(map(int,list(input())))for _ in range(col)]
+    n = int(input())
+    bored = [[0]*n for _ in range(n)]
+    for _ in range(int(input())):
+        y,x = map(int,input().split())
+        bored[y-1][x-1] = 4
+    bored[0][0] = 1
 
-    print(Bfs())
+    d = dict()
+    for _ in range(int(input())):
+        s,c = input().split()
+        d[int(s)] = c
+
+    q = deque([[0,0]])
+    x,y = 0,0
+    cnt,dir = 0,0
+    while 1:
+        cnt+=1
+        x+=xn[dir];y+=yn[dir]
+        if 0>x or x>=n or 0>y or y>=n:break
+        
+        if bored[y][x]==4:
+            bored[y][x] = 1
+            q.append([x,y])
+            if cnt in d:
+                dir = f(d[cnt])
+
+        elif bored[y][x]==0:
+            bored[y][x] = 1
+            q.append([x,y])
+            a,b = q.popleft()
+            bored[b][a] = 0
+            if cnt in d:
+                dir = f(d[cnt])
+        
+        else:break
+    print(cnt)
