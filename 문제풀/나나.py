e38838d7b626e16983284a2c560bff96f2758b9a
Myung-Il <92520262+Myung-Il@users.noreply.github.com>
2023-10-03 21:33:46 +0900

    퀵 스택

--- 문제풀/나나.py
+++ 문제풀/나나.py
@@ -1,12 +1,1 @@
-mn,mx = map(int,input().split())
-
-cnt = mx-mn+1
-l = [1]*cnt
-for i in range(2,int(mx**0.5)+1):
-    sq = mn//i**2
-    while sq*(i**2)<=mx:
-        if 0<=sq*(i**2)-mn<=mx-mn:
-            l[sq*(i**2)-mn] = 0
-        sq += 1
-
-print(sum(l))
+print(10**0.5)
--- 태그/정렬/2___O_nlogn/퀵 스택.py
+++ 태그/정렬/2___O_nlogn/퀵 스택.py
@@ -1,2 +1,27 @@
 l=[4,6,9,12,2,8,11,7,1,3,10,5]
 
+stack = []
+stack.append((0, len(l)-1))
+while stack:
+    start,end = stack.pop()
+
+    pivot_index = (start+end)//2
+    pivot = l[pivot_index]
+
+    left,right = start,end
+    while left <= right:
+        while l[left] < pivot:
+            left += 1
+        while l[right] > pivot:
+            right -= 1
+        if left <= right:
+            l[left], l[right] = l[right], l[left]
+            left += 1
+            right -= 1
+
+    if start < right:
+        stack.append((start, right))
+    if left < end:
+        stack.append((left, end))
+
+    print(l)
