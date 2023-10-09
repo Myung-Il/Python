e5b47de969281c574ce8a74b711837c8edfb365e
Myung-Il <92520262+Myung-Il@users.noreply.github.com>
2023-10-09 21:47:02 +0900

    13904 골드 3

--- 문제풀/나나.py
+++ 문제풀/나나.py
@@ -1,18 +1,13 @@
-from sys import stdin
 import heapq as hq
+from sys import stdin
 input = lambda:stdin.readline().rstrip()
 
 n = int(input())
-
 l = [list(map(int,input().split()))for _ in range(n)]
+
 l.sort()
-
-g = []
-hq.heappush(g, l[0][1])
-for i in range(1,n):
-    if l[i][0]<g[0]:hq.heappush(g,l[i][1])
-    else:
-        hq.heappop(g)
-        hq.heappush(g,l[i][1])
-
-print(len(g))
+q = []
+for d,w in l:
+    hq.heappush(q,w)
+    if d<len(q):hq.heappop(q)
+print(sum(q))
