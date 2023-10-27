1e9a2ef230d8de0cb7592ffe1d9db64a73cf5a70
Myung-Il <ktr040415@gmail.com>
2023-10-27 18:24:30 +0900

    1275 골드 1

--- 문제풀/나나.py
+++ 문제풀/나나.py
@@ -5,32 +5,33 @@ input = lambda:stdin.readline().rstrip()
 def init(node,start,end):
     middle = (start+end)//2
     if start==end:tree[node] = l[start]
-    else:tree[node] = init(node*2,start,middle)*init(node*2+1,middle+1,end)%1_000_000_007
+    else:tree[node] = init(node*2,start,middle)+init(node*2+1,middle+1,end)
     return tree[node]
+
+def rangeS(node,start,end,left,right):
+    middle = (start+end)//2
+    if left>end or start>right:return 0
+    if left<=start and end<=right:return tree[node]
+    return rangeS(node*2,start,middle,left,right)+rangeS(node*2+1,middle+1,end,left,right)
 
 def update(node,start,end,idx,dif):
     middle = (start+end)//2
-    if end<idx or idx<start:return 1
+    if start>idx or idx>end:return
     if start==end:tree[node] = dif
     else:
         update(node*2,start,middle,idx,dif)
         update(node*2+1,middle+1,end,idx,dif)
-        tree[node] = tree[node*2]*tree[node*2+1]%1_000_000_007
-
-def rangeMtp(node,start,end,left,right):
-    middle = (start+end)//2
-    if left>end or start>right:return 1
-    if left<=start and end<=right:return tree[node]
-    return rangeMtp(node*2,start,middle,left,right)*rangeMtp(node*2+1,middle+1,end,left,right)%1_000_000_007
+        tree[node] = tree[node*2]+tree[node*2+1]
 
 
 if __name__=='__main__':
-    n,m,k = map(int,input().split())
-    l = [0]+[int(input())for _ in range(n)]
+    n,k = map(int,input().split())
+    l = [0]+list(map(int,input().split()))
     tree = [0]*n*4
 
     init(1,1,n)
-    for _ in range(m+k):
-        ty,*i = map(int,input().split())
-        if ty==1:update(1,1,n,i[0],i[1])
-        if ty==2:print(rangeMtp(1,1,n,i[0],i[1]))
+    for _ in range(k):
+        a,b,i,c = map(int,input().split())
+        if a>b:print(rangeS(1,1,n,b,a))
+        else:print(rangeS(1,1,n,a,b))
+        update(1,1,n,i,c)
