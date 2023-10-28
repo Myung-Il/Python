aa10ff9934d00b023c4d2d8222f3e5f0d0e22e2d
Myung-Il <ktr040415@gmail.com>
2023-10-28 21:07:10 +0900

    2268 골드 1

--- 문제풀/나나.py
+++ 문제풀/나나.py
@@ -2,12 +2,6 @@ from sys import stdin
 input = lambda:stdin.readline().rstrip()
 
 
-def init(node,start,end):
-    middle = (start+end)//2
-    if start==end:tree[node] = l[start]
-    else:tree[node] = init(node*2,start,middle)+init(node*2+1,middle+1,end)
-    return tree[node]
-
 def rangeS(node,start,end,left,right):
     middle = (start+end)//2
     if left>end or start>right:return 0
@@ -26,12 +20,11 @@ def update(node,start,end,idx,dif):
 
 if __name__=='__main__':
     n,k = map(int,input().split())
-    l = [0]+list(map(int,input().split()))
     tree = [0]*n*4
 
-    init(1,1,n)
     for _ in range(k):
-        a,b,i,c = map(int,input().split())
-        if a>b:print(rangeS(1,1,n,b,a))
-        else:print(rangeS(1,1,n,a,b))
-        update(1,1,n,i,c)
+        t,a,b = map(int,input().split())
+        if t==0:
+            if a>b:print(rangeS(1,1,n,b,a))
+            else:print(rangeS(1,1,n,a,b))
+        else:update(1,1,n,a,b)
