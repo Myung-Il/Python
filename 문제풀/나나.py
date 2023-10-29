0354af8e67f8d4deba1f37dae2a728b14d4f1e79
Myung-Il <ktr040415@gmail.com>
2023-10-29 14:05:17 +0900

    14438 골드 1

--- 문제풀/나나.py
+++ 문제풀/나나.py
@@ -2,11 +2,17 @@ from sys import stdin
 input = lambda:stdin.readline().rstrip()
 
 
+def init(node,start,end):
+    middle = (start+end)//2
+    if start==end:tree[node] = l[start]
+    else:tree[node] = min(init(node*2,start,middle),init(node*2+1,middle+1,end))
+    return tree[node]
+
 def rangeS(node,start,end,left,right):
     middle = (start+end)//2
-    if left>end or start>right:return 0
+    if left>end or start>right:return float('inf')
     if left<=start and end<=right:return tree[node]
-    return rangeS(node*2,start,middle,left,right)+rangeS(node*2+1,middle+1,end,left,right)
+    return min(rangeS(node*2,start,middle,left,right),rangeS(node*2+1,middle+1,end,left,right))
 
 def update(node,start,end,idx,dif):
     middle = (start+end)//2
@@ -15,16 +21,16 @@ def update(node,start,end,idx,dif):
     else:
         update(node*2,start,middle,idx,dif)
         update(node*2+1,middle+1,end,idx,dif)
-        tree[node] = tree[node*2]+tree[node*2+1]
+        tree[node] = min(tree[node*2],tree[node*2+1])
 
 
 if __name__=='__main__':
-    n,k = map(int,input().split())
+    n = int(input())
+    l = [0]+list(map(int,input().split()))
     tree = [0]*n*4
 
-    for _ in range(k):
+    init(1,1,n)
+    for _ in range(int(input())):
         t,a,b = map(int,input().split())
-        if t==0:
-            if a>b:print(rangeS(1,1,n,b,a))
-            else:print(rangeS(1,1,n,a,b))
+        if t==2:print(rangeS(1,1,n,a,b))
         else:update(1,1,n,a,b)
