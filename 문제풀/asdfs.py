6724eef7a50b77147e67a0e10dbc5f2d7354f8ff
Myung-Il <ktr040415@gmail.com>
2023-10-31 19:00:23 +0900

    16975 플레 4

--- 문제풀/asdfs.py
+++ 문제풀/asdfs.py
@@ -1,1 +1,52 @@
-print(max([0,10],[5,0],[float('inf')]))
+from sys import stdin
+input = lambda:stdin.readline().rstrip()
+
+def init(node, start, end):
+    middle = (start+end)//2
+    if start==end:tree[node] = l[start]
+    else:tree[node] = init(node*2, start, middle)+init(node*2+1, middle+1, end)
+    return tree[node]
+
+def lazy(node, start, end):
+    if z[node]:
+        tree[node] += (end-start+1)*z[node]
+        if start!=end:
+            z[node*2] += z[node]
+            z[node*2+1] += z[node]
+        z[node] = 0
+
+def update(node, start, end, left, right, diff):
+    lazy(node, start, end)
+    middle = (start+end)//2
+    if end<left or right<start:return
+    if left<=start and end<=right:
+        tree[node] += (end-start+1)*diff
+        if start!=end:
+            z[node*2] += diff
+            z[node*2+1] += diff
+        return
+    update(node*2, start, middle, left, right, diff)
+    update(node*2+1, middle+1, end, left, right, diff)
+    tree[node] = tree[node*2]+tree[node*2+1]
+
+def rangeSum(node, start, end, x):
+    lazy(node, start, end)
+    middle = (start+end)//2
+    if x<start or end<x:return 0
+    if start==end==x:return tree[node]
+    rs = rangeSum(node*2, start, middle, x)
+    ls = rangeSum(node*2+1, middle+1, end, x)
+    return rs+ls
+
+
+if __name__=='__main__':
+    n = int(input())
+    l = [0]+list(map(int,input().split()))
+    z = [0]*n*4
+    tree = [0]*n*4
+
+    init(1,1,n)
+    for _ in range(int(input())):
+        t,*c = list(map(int,input().split()))
+        if t==1:update(1,1,n,c[0],c[1],c[2])
+        elif t==2:print(rangeSum(1,1,n,c[0]))
--- 문제풀/나나.py
+++ 문제풀/나나.py
@@ -4,24 +4,23 @@ input = lambda:stdin.readline().rstrip()
 
 def init(node,start,end):
     middle = (start+end)//2
-    if start==end:tree[node] = [l[start],start]
+    if start==end:tree[node] = l[start]
     else:tree[node] = min(init(node*2,start,middle),init(node*2+1,middle+1,end))
     return tree[node]
 
 def rangeS(node,start,end,left,right):
     middle = (start+end)//2
-    if left>end or start>right:return [float('inf')]
+    if left>end or start>right:return 0
     if left<=start and end<=right:return tree[node]
     return min(rangeS(node*2,start,middle,left,right),rangeS(node*2+1,middle+1,end,left,right))
 
 def update(node,start,end,idx,dif):
     middle = (start+end)//2
     if start>idx or idx>end:return
-    if start==end:tree[node][0] = dif
+    if start==end:tree[node] = dif
     else:
         update(node*2,start,middle,idx,dif)
         update(node*2+1,middle+1,end,idx,dif)
-        tree[node] = min(tree[node*2],tree[node*2+1])
 
 
 if __name__=='__main__':
