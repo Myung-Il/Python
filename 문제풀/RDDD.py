7235375a5fdf798cbbfe95f3d88f8446870e3e41
Myung-Il <ktr040415@gmail.com>
2023-11-07 10:36:56 +0900

    3034 브론즈 3

--- 문제풀/RDDD.py
+++ 문제풀/RDDD.py
@@ -4,13 +4,4 @@ input = lambda:stdin.readline().rstrip()
 
 
 if __name__=='__main__':
-    n = int(input())
-    q = deque(map(int,input().split()))
-
-    stk = []
-    los = 1
-    while 1:
-        elm = q.popleft()
-        if elm==los:los+=1
-        elif stk and stk[-1]:
-            
+    pass
--- 문제풀/sdfgh.py
+++ 문제풀/sdfgh.py
@@ -1,3 +1,6 @@
-for _ in range(int(input())):
-    v,e = map(int,input().split())
-    print(e-v+2)
+n,w,h = map(int,input().split())
+box = w**2+h**2
+for _ in range(n):
+    m = int(input())
+    if m**2<=box:print('DA')
+    else:print('NE')
