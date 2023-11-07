f5c877a688bc823c9a3b65d7d4f27b16aa79514a
Myung-Il <ktr040415@gmail.com>
2023-11-07 10:21:48 +0900

    3053 브론즈 3

--- 문제풀/RDDD.py
+++ 문제풀/RDDD.py
@@ -2,3 +2,15 @@ from collections import deque
 from sys import stdin
 input = lambda:stdin.readline().rstrip()
 
+
+if __name__=='__main__':
+    n = int(input())
+    q = deque(map(int,input().split()))
+
+    stk = []
+    los = 1
+    while 1:
+        elm = q.popleft()
+        if elm==los:los+=1
+        elif stk and stk[-1]:
+            
---
+++ 문제풀/sdfgh.py
@@ -0,0 +1,5 @@
+from math import pi
+r = int(input())
+
+print(f'{pi*r**2:0.6f}')
+print(f'{2*r**2:0.6f}')
